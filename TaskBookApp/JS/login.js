502cd6fd6b7a1cfb9398716c30a842087693c20f
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2020-02-08 11:14:43 -0800

    Add changes

--- TaskBookApp/JS/login.js
+++ TaskBookApp/JS/login.js
@@ -12,5 +12,5 @@ import config from './modules/config.js';
 const login = document.querySelector( '#login' );
 
 login.addEventListener( 'click', () => {
-    window.location = `${config.authURI}?client_id=${config.clientID}&response_type=${config.response}`;
+    window.location = `${config.authURI}?client_id=${config.clientID}&response_type=${config.responseType}`;
 });
