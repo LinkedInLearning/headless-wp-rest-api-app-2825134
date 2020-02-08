5d3cd22040937cc2408dee76093f6456cfd6bf3c
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2020-02-08 11:15:11 -0800

    Add changes

--- TaskBookApp/JS/modules/config.js
+++ TaskBookApp/JS/modules/config.js
@@ -4,13 +4,13 @@
  * NOT FOR PRODUCTION. PURELY FOR DEMONSTRATION PURPOSES!
  */
 
-const rootURL = '';
+const rootURL = 'http://restapp.local';
 
 const config = {
     rootURL: rootURL,
     taskRoute: `${rootURL}/wp-json/wp/v2/tasks/`,
     authURI: `${rootURL}/oauth/authorize`,
-    clientID: '',
+    clientID: 'EsMj0TRomCYQ2HSp0AialxHT18csceXAl2sznkm0',
     responseType: 'token',
     tokenName: 'taskAppToken'
 }
